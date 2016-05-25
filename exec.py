b8bf225a1097b244e674cebf057a572d56e8aca4
Yohann COUSQUER <ycousquer@couyoh-2.local>
2016-05-25 10:12:12 +0200

    another commit

---
+++ exec.py
@@ -0,0 +1,31 @@
+import prep_file
+
+param = prep_file.read_input_file()
+
+
+#plot CRIV and parameter distribution
+prep_file.CRIV_dist_plot(param)
+
+
+
+
+#Plot model geometry
+prep_file.build_model(param)
+
+#Compute CRIV
+prep_file.compute_CRIV(param)
+
+#Plot regression line
+prep_file.plot_CRIV()
+
+#Get CRIV and R2 from regression line
+prep_file.write_CRIV(param)
+
+#calculate Xfar
+prep_file.compute_Xfar(param)
+
+#Get CRIV distribution from parameter distribution
+prep_file.CRIV_distrib(param)
+
+
+
